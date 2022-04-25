26a621c8cda0051998e6ea4de7f18c5d431ca80a
leminh4000 <leminh2000@gmail.com>
2022-04-25 19:40:58 +0700

    chapter 4 begin

--- app.js
+++ app.js
@@ -1,2 +1,33 @@
 const form=document.querySelector('#task-form');
-console.log(form);
+
+const taskList=document.querySelector('.collection');
+const clearBtn=document.querySelector('.clear-tasks');
+const filter=document.querySelector('#filter');
+console.log(filter);
+
+taskInput=document.querySelector('#task');
+console.log(taskInput);
+
+function addTask(e) {
+    if(taskInput.value===''){
+        alert('Add a task');
+    }
+    const li=document.createElement('li');
+    li.className='collection-item';
+    li.appendChild(document.createTextNode(taskInput.value));
+    const a=document.createElement('a');
+    a.className='delete-item secondary-content';
+    a.innerHTML='<i class="fa fa-remove"></i>';
+    li.appendChild(a);
+    console.log(li);
+    taskList.appendChild(li);
+    taskInput.value='';
+   e.preventDefault();
+}
+
+function loadEventListeners() {
+    form.addEventListener('submit',addTask);
+
+}
+
+loadEventListeners();
